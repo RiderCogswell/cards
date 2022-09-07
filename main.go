86d1b64e67a7e98b631403e61d47fa64966a2dc8
Ridercogswell <ridercogswell@gmail.com>
2022-09-07 14:56:25 -0700

    state

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades" <- this is the same as below
-	card := "Ace of Spades" // only use this syntax when DEFINING a new variable and when we want the type inferred
+	// card := newCard() // only use this syntax when DEFINING a new variable and when we want the type inferred
 
-	fmt.Println(card)
+	// fmt.Println(card)
+	printState()
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
