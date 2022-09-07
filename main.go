438feeef67a9257e9dfe05a447f8d2dd62f51fdf
Ridercogswell <ridercogswell@gmail.com>
2022-09-07 14:48:44 -0700

    return types

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	// var card string = "Ace of Spades" <- this is the same as below
 	card := "Ace of Spades" // only use this syntax when DEFINING a new variable and when we want the type inferred
-	card = "Five"
+
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
