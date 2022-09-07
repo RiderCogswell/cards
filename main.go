86b0d242393be30ee37c997a6a039432e98cb054
Ridercogswell <ridercogswell@gmail.com>
2022-09-07 14:38:51 -0700

    type inference

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	// var card string = "Ace of Spades" <- this is the same as below
+	card := "Ace of Spades" // only use this syntax when DEFINING a new variable and when we want the type inferred
+	card = "Five"
+	fmt.Println(card)
 }
