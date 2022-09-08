1c4c9fae0cd43e8dfeea808bdd889a7fc87a9452
Ridercogswell <ridercogswell@gmail.com>
2022-09-08 13:53:18 -0700

    toString()

--- deck.go
+++ deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // create a new type of 'deck'
 // which is a slice of strings
@@ -33,4 +36,8 @@ func (d deck) print() {
 // this annotation declares that we will be returning two values of type deck
 func deal(d deck, handSize int) (deck, deck) { 
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
--- main.go
+++ main.go
@@ -3,12 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingCards := deal(cards, 5) // this sets two new vars of hand, and remaining, both with types of deck
-
-	// hand.print()
-	// remainingCards.print()
-	greeting := "Hi there!"
-	fmt.Println([]byte(greeting))
+	cards := newDeck()
+	fmt.Println(cards.toString())
 }
