5c360500a3d4960850e273f458bf2f8030eb68dc
Ridercogswell <ridercogswell@gmail.com>
2022-09-07 19:01:05 -0700

    byte slice

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5) // this sets two new vars of hand, and remaining, both with types of deck
+	// hand, remainingCards := deal(cards, 5) // this sets two new vars of hand, and remaining, both with types of deck
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
