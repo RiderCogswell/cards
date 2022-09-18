4045a4248cdb8d27b78776baefb571b7dff8a1bc
Ridercogswell <ridercogswell@gmail.com>
2022-09-17 20:19:36 -0700

    test order

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d) - 1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d) - 1])
+	}
 }
